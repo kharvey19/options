93cfafcb026b336a3b011718537ef92f85790938
TristanBester <tristanbester@gmail.com>
2023-12-13 20:58:10 +0200

    Added env docs

--- src/env/env.py
+++ src/env/env.py
@@ -3,19 +3,49 @@ import numpy as np
 from termcolor import colored
 
 
-class FourRooms(gym.Env):
-    RIGHT = 0
-    LEFT = 1
-    UP = 2
-    DOWN = 3
+class FourRooms(gym.Env[tuple[int, int], np.int32]):
+    """
+    FourRooms environment class.
 
-    def __init__(self) -> None:
-        super().__init__()
+    This class represents a grid-world environment with four rooms.
+    The agent's goal is to navigate from the starting
+    position to the goal position while avoiding obstacles.
+
+    Attributes:
+        RIGHT (int): Action constant representing moving right.
+        LEFT (int): Action constant representing moving left.
+        UP (int): Action constant representing moving up.
+        DOWN (int): Action constant representing moving down.
+
+    Methods:
+        __init__(): Initializes the FourRooms environment.
+        reset(seed=None): Resets the environment to its initial state.
+        step(action): Takes a step in the environment based on the given action.
+        render(): Renders the current state of the environment.
+        _init_map(): Initializes the map of the environment.
+
+    """
+
+    RIGHT: int = 0
+    LEFT: int = 1
+    UP: int = 2
+    DOWN: int = 3
+
+    def __init__(self, **kwargs: dict[any]) -> None:
+        super().__init__(**kwargs)
         self.observation_space = gym.spaces.MultiDiscrete((13, 13))
         self.action_space = gym.spaces.Discrete(4)
         self.max_steps = 10000
+        self.agent_state = (0, 0)
+        self.goal_state = (0, 0)
+        self.step_count = 0
+        self.map = np.zeros((13, 13), dtype=np.int32)
 
-    def reset(self, seed=None):
+    def reset(
+        self,
+        seed: int | None = None,
+        options: dict[str, any] | None = None,
+    ) -> tuple[tuple[int, int], dict[any, any]]:
         self.agent_state = (7, 9)
         self.goal_state = (9, 9)
         self.step_count = 0
@@ -23,7 +53,9 @@ class FourRooms(gym.Env):
         self._init_map()
         return self.agent_state, {}
 
-    def step(self, action):
+    def step(
+        self, action: int
+    ) -> tuple[tuple[int, int], int, bool, bool, dict[any, any]]:
         self.step_count += 1
 
         if action == self.RIGHT:
@@ -39,7 +71,7 @@ class FourRooms(gym.Env):
 
         if self.map[next_state] == 0:
             self.agent_state = next_state
-        reward = self.agent_state == self.goal_state
+        reward = 1 if self.agent_state == self.goal_state else 0
         truncated = self.step_count >= self.max_steps
         terminated = reward == 1 or truncated
         info = {}
